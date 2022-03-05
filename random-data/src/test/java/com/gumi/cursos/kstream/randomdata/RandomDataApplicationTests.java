7e740acb6093d315d0b7b7cada297948379aebb7
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-05 10:58:51 +0100

    [random-data] First commit

---
+++ random-data/src/test/java/com/gumi/cursos/kstream/randomdata/RandomDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumi.cursos.kstream.randomdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
