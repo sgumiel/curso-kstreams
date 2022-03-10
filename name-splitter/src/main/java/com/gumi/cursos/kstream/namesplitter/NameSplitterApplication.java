5e0006b77192ad5ecadea896f19c1136043a42de
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-10 19:17:58 +0100

    [name-spliter] First commit

---
+++ name-splitter/src/main/java/com/gumi/cursos/kstream/namesplitter/NameSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.gumi.cursos.kstream.namesplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameSplitterApplication.class, args);
+	}
+
+}
---
+++ name-splitter/src/test/java/com/gumi/cursos/kstream/namesplitter/NameSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumi.cursos.kstream.namesplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
